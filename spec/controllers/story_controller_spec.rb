d5158a01c2105147ad5c0d9f1b7ef98d773aa37d
Flinn Gendall & Red Hendery <pair+flinnja.redhendery@devbootcamp.com>
2015-10-09 15:26:52 +1300

    failing controller test for update method

--- spec/controllers/story_controller_spec.rb
+++ spec/controllers/story_controller_spec.rb
@@ -24,4 +24,28 @@ RSpec.describe StoryController, type: :controller do
 
   end
 
+  describe "update a story (add a new line)" do
+
+    describe "apply a valid update" do
+      #arrange
+      let!(:stub_story) do
+        allow(Story).to receive(:find) do
+          double('story', id: 1)
+        end
+        allow(Story).to receive(:update) do
+          double('valid story', valid?: true)
+        end
+      end
+
+      let(:update_params) {{newline: "new"}}
+      #act
+      let!(:patch_story) {patch :update, id: 1, story: update_params}
+
+      it 'redirect to update page' do
+        #assert
+        expect(response).to redirect_to(edit_story_path("story"))
+      end
+    end
+  end
+
 end
